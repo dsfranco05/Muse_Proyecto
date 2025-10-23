6eaaa2223d86d60441573d57f52dc90a3a8207ac
dsfranco05@ucatolica.edu.co <dsfranco05@ucatolica.edu.co>
2025-10-22 21:58:27 -0500

    Implementación de base de datos

--- app/database.py
+++ app/database.py
@@ -1,21 +1,28 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mariana123@localhost:5432/muse_db"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql+psycopg2://postgres.lnjufeqfvnacrkdqxypv:MARp3sy0816"
+    "@aws-1-us-east-1.pooler.supabase.com:5432/postgres?sslmode=require"
+)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Función para obtener la sesión de base de datos
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+
+
+
 
 
 
